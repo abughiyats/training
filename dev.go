95d1fb18d857707b36ab12151ac3e54ee49b1810
abughiyats <anggaprabes@gmail.com>
2021-02-18 17:40:42 +0700

    development branch created

---
+++ dev.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("this file development branch only")
+}
--- index.go
+++ index.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println("feature A")
+	fmt.Println("Branch Development")
+	fmt.Println("create new file dev.go")
 
 }
